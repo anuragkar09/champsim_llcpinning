b5ad417d07a9e77b3c1d1aed7885ef617b5b6419
Anurag Kar <anuragkar09@gmail.com>
2024-04-04 23:12:43 -0400

    Added some extra configs and trying to make hawkeye policy work (still non functional)

---
+++ replacement/hawkeye/hawkeye.d
@@ -0,0 +1,40 @@
+replacement/hawkeye/hawkeye.o: replacement/hawkeye/hawkeye.cc inc/cache.h \
+ inc/champsim.h inc/champsim_constants.h inc/util.h inc/delay_queue.hpp \
+ inc/circular_buffer.hpp inc/memory_class.h inc/rowhammer_detector.h \
+ inc/block.h inc/instruction.h inc/trace_instruction.h inc/ooo_cpu.h \
+ inc/operable.h inc/ooo_cpu_modules.inc inc/cache_modules.inc \
+ replacement/hawkeye/hawkeye_predictor.h replacement/hawkeye/optgen.h
+
+inc/cache.h:
+
+inc/champsim.h:
+
+inc/champsim_constants.h:
+
+inc/util.h:
+
+inc/delay_queue.hpp:
+
+inc/circular_buffer.hpp:
+
+inc/memory_class.h:
+
+inc/rowhammer_detector.h:
+
+inc/block.h:
+
+inc/instruction.h:
+
+inc/trace_instruction.h:
+
+inc/ooo_cpu.h:
+
+inc/operable.h:
+
+inc/ooo_cpu_modules.inc:
+
+inc/cache_modules.inc:
+
+replacement/hawkeye/hawkeye_predictor.h:
+
+replacement/hawkeye/optgen.h:
